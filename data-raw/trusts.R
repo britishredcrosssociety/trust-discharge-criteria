896b86f3917bc36e77414c141b93748990de22e3
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-08-31 17:32:28 +0200

    Update trusts to match criteria to resign trusts

--- data-raw/trusts.R
+++ data-raw/trusts.R
@@ -1,4 +1,11 @@
+library(tidyverse)
 library(geographr)
 library(sf)
+
+pkgload::load_all(".")
+
+points_nhs_trusts22 <-
+  points_nhs_trusts22 |>
+  filter(nhs_trust22_code %in% criteria_to_reside$nhs_trust22_code)
 
 usethis::use_data(points_nhs_trusts22, overwrite = TRUE)
